412d04d1bca41db2be4905ae900b824602c5b5b8
vakanto <vakanto@posteo.eu>
2017-12-10 21:25:33 +0100

    DataTypes completed

--- C/DataTypeSize.c
+++ C/DataTypeSize.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-
 int main()
 {
+    int integerValue = 1;
 
+    while(integerValue > 0)
+    {
+        integerValue += 100;
 
+        printf("Value : %d \n", integerValue);
+    }
 
+    printf("Value : %d \n", &integerValue);
 }
